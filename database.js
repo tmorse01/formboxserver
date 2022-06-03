57838ba35e64587e6a14f60df5932dea94ee633b
Taylor <tmorse725@gmail.com>
2022-06-02 23:44:39 -0700

    working saving form data to db

--- database.js
+++ database.js
@@ -3,87 +3,88 @@ const { MongoClient } = require("mongodb");
 var testData = require("./sampledata/tmorsedata.json");
 var arrayOfTestData = require("./sampledata/arrayoftestdata.json");
 
-async function main() {
-  const uri = "mongodb://localhost:27017";
+var _dbClient;
 
-  //create instance of mongo client
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    // await listDatabases(client);
-    // await createFormData(client, testData);
-    // await createMultipleFormData(client, arrayOfTestData);
-    // await findOneFormByUserName(client, "tmorse");
-    await findResponseByFormName(client, "form1");
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-
-main().catch(console.error);
-
-async function createFormData(client, newFormData) {
-  const result = await client
-    .db("formboxdata")
-    .collection("formdata")
-    .insertOne(newFormData);
-  console.log(
-    `New form data created with the following id: ${result.insertedId} `
-  );
-}
-
-async function createMultipleFormData(client, newFormDataArray) {
-  const result = await client
-    .db("formboxdata")
-    .collection("formdata")
-    .insertMany(newFormDataArray);
-  console.log(
-    `${result.insertedCount} new form data created with the following id(s):`
-  );
-  console.log(result.insertedIds);
-}
-
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => {
-    console.log(`- ${db.name}`);
-  });
-}
-
-async function findOneFormByUserName(client, username) {
-  const result = await client
-    .db("formboxdata")
-    .collection("formdata")
-    .findOne({ username: username });
-
-  if (result) {
-    console.log(`Found a form submitted by : ${username}`);
-    console.log(result);
-  } else {
-    console.log(`No results`);
-  }
-}
-
-async function findResponseByFormName(client, formName) {
-  const cursor = await client
-    .db("formboxdata")
-    .collection("formdata")
-    .find({ name: formName });
-
-  const results = await cursor.toArray();
-  if (results) {
-    console.log(`Found results for form : ${formName}`);
-    results.forEach((result, i) => {
-      console.log("result", result);
-      //   console.log(`result ${i}: ${result}`);
+module.exports = {
+  getClient: async function () {
+    // console.log("getClient: ", _dbClient);
+    if (_dbClient) {
+      return _dbClient;
+    } else {
+      console.log("else getClient: ", _dbClient);
+      return await this.connectToServer();
+    }
+  },
+  connectToServer: function () {
+    const uri = "mongodb://localhost:27017";
+    console.log("connectToServer");
+    MongoClient.connect(uri, async function (err, client) {
+      //create instance of mongo client
+      _dbClient = client;
+      console.log("connected to mongodb client: ", _dbClient);
+      // if (callback) return callback(err);
     });
-    return results;
-  } else {
-    console.log(`No results`);
-    return undefined;
-  }
-}
+  },
+  createFormData: async function (newFormData) {
+    console.log("createFormData: ", newFormData);
+    const result = await _dbClient
+      .db("formboxdata")
+      .collection("formdata")
+      .insertOne(newFormData);
+    console.log(
+      `New form data created with the following id: ${result.insertedId} `
+    );
+  },
+  createMultipleFormData: async function (client, newFormDataArray) {
+    const result = await client
+      .db("formboxdata")
+      .collection("formdata")
+      .insertMany(newFormDataArray);
+    console.log(
+      `${result.insertedCount} new form data created with the following id(s):`
+    );
+    console.log(result.insertedIds);
+  },
+
+  // async function listDatabases(client) {
+  //   const databasesList = await client.db().admin().listDatabases();
+  //   console.log("Databases:");
+  //   databasesList.databases.forEach((db) => {
+  //     console.log(`- ${db.name}`);
+  //   });
+  // }
+
+  // async function findOneFormByUserName(client, username) {
+  //   const result = await client
+  //     .db("formboxdata")
+  //     .collection("formdata")
+  //     .findOne({ username: username });
+
+  //   if (result) {
+  //     console.log(`Found a form submitted by : ${username}`);
+  //     console.log(result);
+  //   } else {
+  //     console.log(`No results`);
+  //   }
+  // }
+
+  // async function findResponseByFormName(client, formName) {
+  //   const cursor = await client
+  //     .db("formboxdata")
+  //     .collection("formdata")
+  //     .find({ name: formName });
+
+  //   const results = await cursor.toArray();
+  //   if (results) {
+  //     console.log(`Found results for form : ${formName}`);
+  //     results.forEach((result, i) => {
+  //       console.log("result", result);
+  //       //   console.log(`result ${i}: ${result}`);
+  //     });
+  //     return results;
+  //   } else {
+  //     console.log(`No results`);
+  //     return undefined;
+  //   }
+  // }
+};
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const database = require("./database");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/connectToDb", (req, res) => {
+  console.log("CONNECT TO DB");
+  database.connectToServer();
+  // var dbClient = database.getClient();
+  // console.log("dbClient on connect: ", dbClient);
+  res.json({ message: "Connected to mongodb client" });
+});
+
+app.put("/submitFormValues", (req, res) => {
+  console.log("submitFormValues", req.body);
+  // console.log("submitFormValues dbClient: ", dbClient);
+  database.createFormData(req.body);
+  // res.json({ message: "Hello from server!" });
 });
 
 app.get("/", (req, res) => {
