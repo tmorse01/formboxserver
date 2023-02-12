f9af291c73763fa4bf68c2600327e51e56e5e932
Taylor <tmorse725@gmail.com>
2023-02-11 19:34:49 -0800

    updates to point at static webpage

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("views"));
 
 // database.connectToServer();
 
